1e4da2730ebaaa84e7283bd25fa15958ddd1d966
KyryloYekymov <81637731+KyryloYekymov@users.noreply.github.com>
2021-04-25 16:25:07 +0300

    Started carousel on gift-sets, need finishing

--- js/index.js
+++ js/index.js
@@ -15,3 +15,44 @@ span.onclick = function() {
     menuBtn.classList.remove('open');
     menuOpen = false;
   }
+
+
+// carousel gift-sets
+
+let slideIndex = 1; // start index of the carousel
+showSlides(slideIndex);
+ 
+function plusSlide() {
+    showSlides(slideIndex += 1); // click on the "Next" button, goes to the next slide
+}
+ 
+function minusSlide() {
+    showSlides(slideIndex -= 1); // click on the "Prev" button, go to the previos slide
+}
+ 
+function currentxSlide(n) {
+    showSlides(slideIndex = n); // function invoked by clicking on dots, goes to the n-th slide
+}
+ 
+function showSlides(n) {
+    const slides = document.getElementsByClassName("giftSet"); // creates an array with all slides
+    const dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1; // goest to the first slide when the last slide is reached
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+document.querySelector('.prev').addEventListener('click', minusSlide);
+document.querySelector('.next').addEventListener('click', plusSlide);
