b8e3be016a059578f1c91638b1815fcdd4fdfdeb
KyryloYekymov <81637731+KyryloYekymov@users.noreply.github.com>
2021-04-26 18:08:45 +0300

    Fixed minor mistakes

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@ const menuBtn = document.querySelector('.menu-btn');
 const modal = document.querySelector('.modal');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    modal.style.display = "block";
-    menuOpen = true;
-  }
+if (!menuOpen) {
+  menuBtn.classList.add('open');
+  modal.style.display = "block";
+  menuOpen = true;
+}
 });
 
-let span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
+let closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function() {
     modal.style.display = "none";
     menuBtn.classList.remove('open');
     menuOpen = false;
-  }
+}
