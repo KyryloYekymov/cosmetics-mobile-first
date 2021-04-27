2b1a8f2d0a5e4f5bc0838a049e4651acfcda2c12
KyryloYekymov <81637731+KyryloYekymov@users.noreply.github.com>
2021-04-27 18:10:46 +0300

    Fixed minor mistakes, next: disable JavaSript when screen width > 768

--- js/index.js
+++ js/index.js
@@ -32,8 +32,12 @@ function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("giftSet");
   const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
