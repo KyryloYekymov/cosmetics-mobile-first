8e0a1f7b1d8a1adce43b9e2f097e2646b8964537
KyryloYekymov <81637731+KyryloYekymov@users.noreply.github.com>
2021-04-27 18:03:17 +0300

    Added working carousel, next: disable JavaScript when screen width > 768

--- js/index.js
+++ js/index.js
@@ -15,3 +15,31 @@ closeBtn.onclick = function() {
     menuBtn.classList.remove('open');
     menuOpen = false;
 }
+
+// Carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("giftSet");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
